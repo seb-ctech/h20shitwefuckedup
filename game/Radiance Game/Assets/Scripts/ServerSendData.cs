d75b6da3a183d74a27b61358a70bea39af4d4c99
Seb W <thelayeredmind@gmail.com>
2022-01-16 14:32:11 +0000

    Simple Socket Communication

---
+++ game/Radiance Game/Assets/Scripts/ServerSendData.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections; 
+using System.Collections.Generic; 
+using System.Net; 
+using System.Net.Sockets; 
+using System.Text; 
+using System.Threading; 
+using UnityEngine;  
+
+/* 
+
+This is the Network component that Allows other Applications to connect and request data.
+
+*/
+public class ServerSendData : MonoBehaviour {  	
+	#region private members 	
+	private TcpListener tcpListener; 
+	private Thread tcpListenerThread;  	
+	private TcpClient connectedTcpClient; 	
+	#endregion 	
+		
+	void Start () { 		
+		// Start TcpServer background thread 		
+		tcpListenerThread = new Thread (new ThreadStart(ListenForIncommingRequests)); 		
+		tcpListenerThread.IsBackground = true; 		
+		tcpListenerThread.Start(); 	
+	}  	
+	
+	// Update is called once per frame
+	void Update () { 		
+		if (Input.GetKeyDown(KeyCode.Space)) {             
+			SendMessage();         
+		} 	
+	}  	
+	
+	private void ListenForIncommingRequests () { 		
+		try { 			
+			// Create listener on localhost port 8052. 			
+			tcpListener = new TcpListener(IPAddress.Parse("127.0.0.1"), 8052); 			
+			tcpListener.Start();              
+			Debug.Log("Server is listening");              
+			Byte[] bytes = new Byte[1024];  			
+			while (true) { 				
+				using (connectedTcpClient = tcpListener.AcceptTcpClient()) { 					
+					// Get a stream object for reading 					
+					using (NetworkStream stream = connectedTcpClient.GetStream()) { 						
+						int length; 						
+						// Read incomming stream into byte arrary. 						
+						while ((length = stream.Read(bytes, 0, bytes.Length)) != 0) { 							
+							var incommingData = new byte[length]; 							
+							Array.Copy(bytes, 0, incommingData, 0, length);  							
+							// Convert byte array to string message. 							
+							string clientMessage = Encoding.ASCII.GetString(incommingData); 							
+							Debug.Log("client message received as: " + clientMessage); 						
+						} 					
+					} 				
+				} 			
+			} 		
+		} 		
+		catch (SocketException socketException) { 			
+			Debug.Log("SocketException " + socketException.ToString()); 		
+		}     
+	}  	
+	/// <summary> 	
+	/// Send message to client using socket connection. 	
+	/// </summary> 	
+	private void SendMessage() { 		
+		if (connectedTcpClient == null) {             
+			return;         
+		}  		
+		
+		try { 			
+			// Get a stream object for writing. 			
+			NetworkStream stream = connectedTcpClient.GetStream(); 			
+			if (stream.CanWrite) {                 
+				string serverMessage = "This is a message from your server."; 			
+				// Convert string message to byte array.                 
+				byte[] serverMessageAsByteArray = Encoding.ASCII.GetBytes(serverMessage); 				
+				// Write byte array to socketConnection stream.               
+				stream.Write(serverMessageAsByteArray, 0, serverMessageAsByteArray.Length);               
+				Debug.Log("Server sent his message - should be received by client");           
+			}       
+		} 		
+		catch (SocketException socketException) {             
+			Debug.Log("Socket exception: " + socketException);         
+		} 	
+	} 
+}
