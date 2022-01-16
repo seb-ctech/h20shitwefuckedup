ff2eb84d2d0672205cc6ab6a1e83cdc91f0af709
Seb W <thelayeredmind@gmail.com>
2022-01-16 15:10:12 +0000

    Change Data sent by Server to float

--- visualization/Radiance Visualization/Assets/Scripts/ClientReceiveData.cs
+++ visualization/Radiance Visualization/Assets/Scripts/ClientReceiveData.cs
@@ -21,13 +21,6 @@ public class ClientReceiveData : MonoBehaviour {
 	void Start () {
 		ConnectToTcpServer();     
 	}  	
-	// Update is called once per frame
-	void Update () {         
-		if (Input.GetKeyDown(KeyCode.Space)) {             
-			SendMessage();         
-		}     
-	}  	
-
 	private void ConnectToTcpServer () { 		
     // Create and start a Thread to Listen for Data
 		try {  			
@@ -42,18 +35,18 @@ public class ClientReceiveData : MonoBehaviour {
 	private void ListenForData() { 		
 		try { 			
 			socketConnection = new TcpClient("localhost", 8052);  			
-			Byte[] bytes = new Byte[1024];             
+			Byte[] bytes = new Byte[4];             
 			while (true) { 				
 				// Get a stream object for reading 				
 				using (NetworkStream stream = socketConnection.GetStream()) { 					
 					int length; 					
 					// Read incomming stream into byte arrary. 					
 					while ((length = stream.Read(bytes, 0, bytes.Length)) != 0) { 						
-						var incommingData = new byte[length]; 						
-						Array.Copy(bytes, 0, incommingData, 0, length); 						
-						// Convert byte array to string message. 						
-						string serverMessage = Encoding.ASCII.GetString(incommingData); 						
-						Debug.Log("server message received as: " + serverMessage); 					
+						var incomingData = new byte[length]; 						
+						Array.Copy(bytes, 0, incomingData, 0, length); 						
+						// Convert byte array to string message. 				
+						float serverData = BitConverter.ToSingle(incomingData, 0); 						
+						Debug.Log("server message received as: " + serverData); 					
 					} 				
 				} 			
 			}         
@@ -62,27 +55,4 @@ public class ClientReceiveData : MonoBehaviour {
 			Debug.Log("Socket exception: " + socketException);         
 		}     
 	}  	
-	/// <summary> 	
-	/// Send message to server using socket connection. 	
-	/// </summary> 	
-	private void SendMessage() {         
-		if (socketConnection == null) {             
-			return;         
-		}  		
-		try { 			
-			// Get a stream object for writing. 			
-			NetworkStream stream = socketConnection.GetStream(); 			
-			if (stream.CanWrite) {                 
-				string clientMessage = "This is a message from one of your clients."; 				
-				// Convert string message to byte array.                 
-				byte[] clientMessageAsByteArray = Encoding.ASCII.GetBytes(clientMessage); 				
-				// Write byte array to socketConnection stream.                 
-				stream.Write(clientMessageAsByteArray, 0, clientMessageAsByteArray.Length);                 
-				Debug.Log("Client sent his message - should be received by server");             
-			}         
-		} 		
-		catch (SocketException socketException) {             
-			Debug.Log("Socket exception: " + socketException);         
-		}     
-	} 
 }
