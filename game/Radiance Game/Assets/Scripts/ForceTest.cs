76e8af26206d83d32f50a5f82c16688f74f5f0b9
kn03t3r1ch <grey_lucas@hotmail.de>
2022-02-14 17:46:50 +0100

    refined the physics for the torae

--- game/Radiance Game/Assets/Scripts/ForceTest.cs
+++ game/Radiance Game/Assets/Scripts/ForceTest.cs
@@ -8,21 +8,27 @@ public class ForceTest : MonoBehaviour
 
     //private Variables
     private Rigidbody rb;
-    private bool buttonPress_up;
-    private bool buttonPress_left;
-    private bool buttonPress_right, buttonPress_down;
+
+    private bool buttonPress_right, buttonPress_down, buttonPress_left, buttonPress_up;
     private bool zn_1, zn_2, zn_3, zn_4;
+    private float upForce_strength_noise;
+    private bool n_1, n_2, n_3, n_4 = true;
+    private bool buttonPushed;
     private ButtonEvent button;
     private bool init = false;
     private GameObject buttonHandler;
 
-    private float upForce = 100.0f;
+    [Range(-10.0f, 50.0f)]
+    public float randomNoise_1, randomNoise_2;
+
+
+    private float buttonUpForce = 100.0f;
+    private float upForce = 5.0f;
     // Start is called before the first frame update
     public float noiseFactor;
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
-        Debug.Log(gameObject.name + " " + rb);
     }
 
 
@@ -69,33 +75,23 @@ public class ForceTest : MonoBehaviour
     {
         if (zn_1)
         {
+            if (value < 0 || value > 0 && buttonPushed)
+            {
+                upForce_strength_noise = Random.Range(-30.0f, 80.0f);
 
-            rb.AddForce(0, upForce * value, 0);
+                buttonPushed = false;
+            }
+            rb.AddForce(upForce_strength_noise, buttonUpForce * value, upForce_strength_noise);
+            rb.AddTorque(1.0f, upForce_strength_noise / 6, 4.0f);
         }
     }
 
     void ControlForceByArrowKeys()
     {
-        if (buttonPress_up == true && zn_1)
-        {
-            rb.AddForce(0, upForce, 0);
-            buttonPress_up = false;
-        }
-        if (buttonPress_left && zn_2)
-        {
-            rb.AddForce(0, upForce, 0);
-            buttonPress_left = false;
-        }
-        else if (buttonPress_right && zn_3)
-        {
-            rb.AddForce(0, upForce, 0);
-            buttonPress_right = false;
-        }
-        else if (buttonPress_down && zn_4)
-        {
-            rb.AddForce(0, upForce, 0);
-            buttonPress_down = false;
-        }
+        pressedAnyButton(ref buttonPress_up, zn_1, n_1, 1.0f, -1.0f);
+        pressedAnyButton(ref buttonPress_left, zn_2, n_2, -1.0f, -1.0f);
+        pressedAnyButton(ref buttonPress_right, zn_3, n_3, 1.0f, 1.0f);
+        pressedAnyButton(ref buttonPress_down, zn_4, n_4, -1.0f, 1.0f);
     }
 
     void InitializeButtonControls()
@@ -107,6 +103,29 @@ public class ForceTest : MonoBehaviour
         buttonHandler = GameObject.Find("ButtonHandler");
         button = buttonHandler.GetComponent<ButtonEventDispatcher>().GetEvent();
         button.AddListener(PushButton);
+    }
+
+
+
+    void pressedAnyButton(ref bool buttonKey, bool zn, bool noise, float x, float z)
+    {
+        float toraeHeight;
+        if (buttonKey && zn)
+        {
+            if (noise)
+            {
+                upForce_strength_noise = Random.Range(randomNoise_1, randomNoise_2);
+                noise = false;
+            }
+            rb.AddForce(x * upForce_strength_noise, upForce, z * upForce_strength_noise / 3);
+
+            toraeHeight = transform.position.y;
+            if (toraeHeight > 3)
+            {
+                rb.AddTorque(1.0f, upForce_strength_noise / 6, 4.0f);
+            }
+            buttonKey = false;
+        }
     }
 
     void OnTriggerEnter(Collider zone)
@@ -137,18 +156,23 @@ public class ForceTest : MonoBehaviour
         {
             Debug.Log("Collision exit " + zone.gameObject.name);
             zn_1 = false;
+            buttonPushed = true;
+            n_1 = true;
         }
         else if (zone.gameObject.name == "Zone_2")
         {
             zn_2 = false;
+            n_2 = true;
         }
         else if (zone.gameObject.name == "Zone_3")
         {
             zn_3 = false;
+            n_3 = true;
         }
         else if (zone.gameObject.name == "Zone_4")
         {
             zn_4 = false;
+            n_4 = true;
         }
     }
 
